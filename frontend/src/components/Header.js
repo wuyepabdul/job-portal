f9643beffd0163261f062a328af6ac2eb11a94ba
wuyepabdul <wuyepabdul@gmail.com>
2023-07-04 23:20:22 +0100

    Add background design to Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,22 @@
+import { Box, styled } from "@mui/material";
 import React from "react";
+import headerImage from "../images/jobbg.jpg";
 
 const Header = () => {
-  return <div>Header</div>;
+  const StyleHeader = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "center",
+    minHeight: 400,
+    backgroundImage: `url(${headerImage})`,
+    backgroundSize: `cover`,
+    backgroundPosition: "center",
+    backgroundColor: theme.palette.secondary.main,
+  }));
+  return (
+    <>
+      <StyleHeader />
+    </>
+  );
 };
 
 export default Header;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Header />
       Home Page
     </>
   );

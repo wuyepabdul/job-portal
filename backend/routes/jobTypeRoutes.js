360dbfa69503f76a37cdfa30aa650b3d519c81a7
wuyepabdul <wuyepabdul@gmail.com>
2023-07-05 21:46:26 +0100

    Add all job types routes

--- backend/routes/jobTypeRoutes.js
+++ backend/routes/jobTypeRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { isAuthenticated } = require("../middleware/auth");
-const { createJobTypeController } = require("../controller/jobTypeController");
+const { createJobTypeController, allJobTypesController } = require("../controller/jobTypeController");
 
 const router = express.Router();
 
 router.post("/type/create", isAuthenticated, createJobTypeController);
+router.post("/type/jobs", isAuthenticated, allJobTypesController);
 
 module.exports = router;

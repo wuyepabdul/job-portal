f9a85a61015c7e0db405c62c262a36eba896856a
wuyepabdul <wuyepabdul@gmail.com>
2023-07-05 21:33:22 +0100

    Create jobType controller file and add create job type controller

---
+++ backend/controller/jobTypeController.js
@@ -0,0 +1,15 @@
+const JobTypeModel = require("../models/jobTypeModel");
+
+exports.createJobType = async (req, res) => {
+  try {
+    const jobType = await JobTypeModel.create({
+      jobTypeName: req.body.jobTypeName,
+      user: req.user.id,
+    });
+
+    return res.status(201).json({ success: true, jobType });
+  } catch (error) {
+    console.log("Create job error", error.message);
+    return res.status(500).json({ error: true, message: "Server error" });
+  }
+};

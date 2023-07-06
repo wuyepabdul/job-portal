80bf837be7ac0970e8774837b6b7008eb22c2d8a
wuyepabdul <wuyepabdul@gmail.com>
2023-07-06 23:50:00 +0100

    Add get singleJob route

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { isAuthenticated } = require("../middleware/auth");
-const { createJobController } = require("../controller/jobController");
+const {
+  createJobController,
+  singleJobController,
+} = require("../controller/jobController");
 
 const router = express.Router();
 
 router.post("/job/create", isAuthenticated, createJobController);
+router.get("/job/:id", singleJobController);
 
 module.exports = router;

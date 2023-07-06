ce5f9baab498430ea08f668c4776f4f57f7ffbb9
wuyepabdul <wuyepabdul@gmail.com>
2023-07-07 00:00:01 +0100

    Add job update route

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const { isAuthenticated } = require("../middleware/auth");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
 const {
   createJobController,
   singleJobController,
+  updateJobController,
 } = require("../controller/jobController");
 
 const router = express.Router();
 
-router.post("/job/create", isAuthenticated, createJobController);
+router.post("/job/create", isAuthenticated, isAdmin, createJobController);
 router.get("/job/:id", singleJobController);
+router.put(
+  "/job/update/:job_id",
+  isAuthenticated,
+  isAdmin,
+  updateJobController
+);
 
 module.exports = router;

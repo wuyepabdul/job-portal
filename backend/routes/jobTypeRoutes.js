7c31155addf28599b203e6e6a150f69b86270ff9
wuyepabdul <wuyepabdul@gmail.com>
2023-07-09 12:49:52 +0100

    Add updateJobType route

--- backend/routes/jobTypeRoutes.js
+++ backend/routes/jobTypeRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { isAuthenticated } = require("../middleware/auth");
-const { createJobTypeController, allJobTypesController } = require("../controller/jobTypeController");
+const { isAuthenticated, isAdmin } = require("../middleware/auth");
+const {
+  createJobTypeController,
+  allJobTypesController,
+  updateJobTypeController,
+} = require("../controller/jobTypeController");
 
 const router = express.Router();
 
 router.post("/type/create", isAuthenticated, createJobTypeController);
 router.post("/type/jobs", isAuthenticated, allJobTypesController);
-
+router.put(
+  "/type/update/:type_id",
+  isAuthenticated,
+  isAdmin,
+  updateJobTypeController
+);
 module.exports = router;

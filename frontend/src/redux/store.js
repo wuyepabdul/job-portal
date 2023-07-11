d6a03dec3c69924a71347ce0342bca60b842d261
wuyepabdul <wuyepabdul@gmail.com>
2023-07-11 14:59:37 +0100

    Add loadJobType reducer in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { loadJobReducer } from "./reducers/jobReducers";
+import { loadJobTypeReducer } from "./reducers/jobTypeReducers";
 
 const reducer = combineReducers({
   loadJobs: loadJobReducer,
+  jobType: loadJobTypeReducer,
 });
 
 let initialState = {};

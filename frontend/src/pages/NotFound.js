ae56b6a03aeed6ca775e7370fb280a02de888f37
wuyepabdul <wuyepabdul@gmail.com>
2023-07-12 22:09:42 +0100

    Style Not Found page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { Box } from "@mui/material";
+import Footer from "../components/Footer";
 
 const NotFound = () => {
-  return <h1>Page Not Found</h1>;
+  return (
+    <>
+      <Navbar />
+      <Box
+        sx={{
+          height: "81vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>Page Not Found</h1>
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default NotFound;

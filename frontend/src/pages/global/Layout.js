8e81a98c425c0b434d3f09724a97d94a9f1fe002
wuyepabdul <wuyepabdul@gmail.com>
2023-07-14 00:12:18 +0100

    Add HeaderTop component

--- frontend/src/pages/global/Layout.js
+++ frontend/src/pages/global/Layout.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import React from "react";
 import SidebarAdmin from "./SidebarAdmin";
+import HeaderTop from "./HeaderTop";
 
 const Layout = (Component)=>(   {...props}) => {
   return <>

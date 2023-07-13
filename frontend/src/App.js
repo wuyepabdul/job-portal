de8f6e7d3a2a47e817e44ead4a4e0597c2524278
wuyepabdul <wuyepabdul@gmail.com>
2023-07-14 00:20:24 +0100

    Add Admin dashboard layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,14 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ProSidebarProvider } from "react-pro-sidebar";
 import Login from "./pages/Login";
 import "./App.css";
 import UserDashboard from "./pages/user/UserDashboard";
 import UserRoute from "./components/UserRoute";
+import Layout from "./pages/global/Layout";
+
+const UserDashboardHOC = Layout(UserDashboard);
 
 const App = () => {
   return (
@@ -17,24 +21,26 @@ const App = () => {
       <ToastContainer />
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/user/dashboard"
-              element={
-                <UserRoute>
-                  {" "}
-                  <UserDashboard />
-                </UserRoute>
-              }
-            />
-            <Route path="/search/location/:location" element={<Home />} />
-            <Route path="/search/:keyword" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <ProSidebarProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/user/dashboard"
+                element={
+                  <UserRoute>
+                    {" "}
+                    <UserDashboardHOC />
+                  </UserRoute>
+                }
+              />
+              <Route path="/search/location/:location" element={<Home />} />
+              <Route path="/search/:keyword" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ProSidebarProvider>
       </ThemeProvider>
     </>
   );

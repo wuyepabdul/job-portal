d8f76bf27712ef14f997467614073f60d5979ff3
wuyepabdul <wuyepabdul@gmail.com>
2023-07-13 10:11:36 +0100

    Add logout Action

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import {
+  USER_LOGOUT_FAIL,
+  USER_LOGOUT_REQUEST,
+  USER_LOGOUT_SUCCESS,
   USER_SIGNIN_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
@@ -15,5 +18,18 @@ export const userSigninAction = (user) => async (dispatch) => {
   } catch (error) {
     dispatch({ type: USER_SIGNIN_FAIL, payload: error.response.data.message });
     toast.error(error.response.data.message);
+  }
+};
+
+export const userLogoutAction = () => async (dispatch) => {
+  dispatch({ type: USER_LOGOUT_REQUEST });
+
+  try {
+    const { data } = await axios.get("/api/logout");
+    dispatch({ type: USER_LOGOUT_SUCCESS, payload: data });
+    toast.success("Logout Successfull");
+  } catch (error) {
+    dispatch({ type: USER_LOGOUT_FAIL, payload: error.response.data.message });
+    toast.error("Logout Fail");
   }
 };

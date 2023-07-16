ff2e535cb64b8f0aff50db6433d487952bb17eb1
wuyepabdul <wuyepabdul@gmail.com>
2023-07-16 13:31:56 +0100

    Add userinfo display layout style

--- frontend/src/pages/user/UserInfoDashboard.js
+++ frontend/src/pages/user/UserInfoDashboard.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Box, Card, CardContent, Typography, useTheme } from "@mui/material";
+import React from "react";
 
 const UserInfoDashboard = () => {
-  return (
-    <div>UserInfoDashboard</div>
-  )
-}
+  const palette = useTheme();
 
-export default UserInfoDashboard
+  return (
+    <Box sx={{ maxWidth: "50%", margin: "auto", pt: "10" }}>
+      <Card sx={{ minWidth: 275, bgcolor: palette.secondary.midNightBlue }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 16, color: "#fafafa" }} gutterBottom>
+            Personal info
+          </Typography>
+          <hr style={{ marginBottom: "30px" }} />
+          <Typography variant="h6" component="div" sx={{ color: "#fafafa" }}>
+            First Name: John
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ color: "#fafafa" }}>
+            Last Name: Doe
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ color: "#fafafa" }}>
+            Email: johndoe@gmail.com
+          </Typography>
+          <Typography
+            sx={{ mb: 1.5, color: "grey", pt: 2 }}
+            color="text.secondary"
+          >
+            Status: Regular User
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default UserInfoDashboard;

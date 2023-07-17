0466efba8f8d79e9d654b3e5d12a096cd3274be4
wuyepabdul <wuyepabdul@gmail.com>
2023-07-17 11:49:31 +0100

    Add StateComponent

---
+++ frontend/src/components/StateComponent.js
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  IconButton,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React from "react";
+
+const StateComponent = ({ value, icon, description, money }) => {
+  const { palette } = useTheme();
+
+  return (
+    <Card sx={{ bgcolor: palette.midNightBlue, width: "100%" }}>
+      <CardContent>
+        <IconButton sx={{ bgcolor: palette.primary.main, mb: 2 }}>
+          {icon}
+        </IconButton>
+        <Typography
+          variant="h4"
+          sx={{ color: "#fafafa", mb: "1px", fontWeight: 700 }}
+        >
+          {money !== "" ? money + value : value}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "#fafafa", mb: 0 }}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default StateComponent;

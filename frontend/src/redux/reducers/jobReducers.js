5945d3dbd2a81fede8b8c8a0a669db8181a0065d
wuyepabdul <wuyepabdul@gmail.com>
2023-07-17 16:57:37 +0100

    Add loadsingleReducer

--- frontend/src/redux/reducers/jobReducers.js
+++ frontend/src/redux/reducers/jobReducers.js
@@ -3,6 +3,10 @@ import {
   JOB_LOAD_REQUEST,
   JOB_LOAD_RESET,
   JOB_LOAD_SUCCESS,
+  SINGLE_JOB_LOAD_FAIL,
+  SINGLE_JOB_LOAD_REQUEST,
+  SINGLE_JOB_LOAD_RESET,
+  SINGLE_JOB_LOAD_SUCCESS,
 } from "../constants/jobConstants";
 
 export const loadJobReducer = (state = { jobs: [] }, action) => {
@@ -31,3 +35,21 @@ export const loadJobReducer = (state = { jobs: [] }, action) => {
   }
 };
 
+export const loadSingleJobReducer = (state = { singleJob: {} }, action) => {
+  switch (action.type) {
+    case SINGLE_JOB_LOAD_REQUEST:
+      return { loading: true };
+    case SINGLE_JOB_LOAD_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        singleJob: action.payload.singleJob,
+      };
+    case SINGLE_JOB_LOAD_FAIL:
+      return { loading: false, error: action.payload };
+    case SINGLE_JOB_LOAD_RESET:
+      return {};
+    default:
+      return state;
+  }
+};

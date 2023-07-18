1036f6e3574b74ef2f98a814a05b22de0766ae77
wuyepabdul <wuyepabdul@gmail.com>
2023-07-18 23:59:40 +0100

    Add id filed in table

--- frontend/src/pages/admin/DashJobs.js
+++ frontend/src/pages/admin/DashJobs.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { DataGrid, GridToolbar, gridClasses } from "@mui/x-data-grid";
+import AddIcon from "@mui/icons-material/Add";
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import { jobLoadAction } from "../../redux/actions/jobActions";
 
 const DashJobs = () => {
-  return (
-    <div>DashJobs</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default DashJobs
+  useEffect(() => {
+    dispatch(jobLoadAction());
+  }, [dispatch]);
+
+  const { jobs, loading } = useSelector((state) => state.loadJobs);
+  let data = [];
+  data = jobs !== undefined && jobs.length > 0 ? jobs : [];
+
+  const deleteJobById = (e, id) => {
+    console.log(e, id);
+  };
+
+  const columns = [
+    { field: "_id", headerName: "Job ID", width: 150, editable: true },
+   
+  ];
+  return (
+    <>
+      <Box>
+        <Typography variant="h4" sx={{ color: "white", pb: 3 }}>
+          Jobs List
+        </Typography>
+        <Box sx={{ pb: 2, display: "flex", justifyContent: "right" }}>
+          <Button variant="contained" color="success" startIcon={<AddIcon />}>
+            Create Job
+          </Button>
+        </Box>
+        <Paper sx={{ bgcolor: "midNightBlue" }}>
+          <Box sx={{ height: 400, width: "100%" }}>
+            <DataGrid
+              sx={{
+                "& .MuiTablePagination-displayedRows": { color: "white" },
+                color: "white",
+                [`& .${gridClasses.row}`]: {
+                  bgcolor: (theme) => theme.palette.secondary.main,
+                },
+                button: {
+                  color: "#ffffff",
+                },
+              }}
+              getRowId={(row) => row._id}
+              rows={data}
+              columns={columns}
+              pageSize={3}
+              rowsPerPageOptions={[3]}
+              checkboxSelection
+              slots={{ toolbar: GridToolbar }}
+            />
+          </Box>
+        </Paper>
+      </Box>
+    </>
+  );
+};
+
+export default DashJobs;
